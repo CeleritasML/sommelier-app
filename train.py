d2e36b47cced7c7977622e0fe8079ece529e0752
Jingsong Gao <jg2109@georgetown.edu>
2022-09-08 11:18:14 -0400

    Add checkpoint callbacks.

--- train.py
+++ train.py
@@ -7,12 +7,19 @@ from src.model import WineBert
 pl.seed_everything(42)
 
 dm = WineDataModule()
-
 model = WineBert()
 
+checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    save_top_k=10,
+    monitor="val_loss",
+    mode="min",
+    filename="ckpt-{epoch:02d}-{val_loss:.2f}-{val_accuracy:.4f}",
+)
+
 trainer = pl.Trainer(
-    max_epochs=3,
+    max_epochs=50,
     accelerator="auto",
     devices=1 if torch.cuda.is_available() else None,
+    callbacks=[checkpoint_callback],
 )
 trainer.fit(model, datamodule=dm)

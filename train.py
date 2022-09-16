60c4255109342c0629b536dc05101959aee25f54
jingsong.gao <jingsong.gao@bytedance.com>
2022-09-16 00:26:27 -0400

    Add early stopping callback.

--- train.py
+++ train.py
@@ -16,10 +16,15 @@ checkpoint_callback = pl.callbacks.ModelCheckpoint(
     filename="ckpt-{epoch:02d}-{val_loss:.2f}-{val_accuracy:.4f}",
 )
 
+early_stop_callback = pl.callbacks.EarlyStopping(
+    monitor="val_accuracy", min_delta=0.1, patience=3, verbose=False, mode="max"
+)
+
 trainer = pl.Trainer(
     max_epochs=50,
     accelerator="auto",
     devices=1 if torch.cuda.is_available() else None,
-    callbacks=[checkpoint_callback],
+    callbacks=[checkpoint_callback, early_stop_callback],
 )
 trainer.fit(model, datamodule=dm)
+trainer.test(model, datamodule=dm)
